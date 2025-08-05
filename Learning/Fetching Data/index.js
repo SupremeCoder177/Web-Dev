b82bff171ac745581adb2ec2631e3a163b79b7e4
Skullfire <onlyforworkplsremember@gmail.com>
2025-08-05 23:34:51 +0530

    Learned Styling and fetching

--- Learning/Fetching Data/index.js
+++ Learning/Fetching Data/index.js
@@ -1,4 +1,33 @@
-const dat = fetch("data.json")
-.then(res => res.json());
+const _data = null;
 
-console.log(dat);
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("data.json")
+    .then(res => {
+        if(!res.ok) throw new Error("Data not found");
+        return res.json();
+        }
+    )
+    .then(data => {
+        const table_body = document.querySelector("#table tbody");
+
+        data.forEach(user => {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${user.name}</td>
+            <td>${user.age}</td>
+            `
+            table_body.append(row);
+        }
+    );
+    }).catch(err => console.log("Failed to load data.", err))
+})
+
+const addBtn = document.getElementById("add");
+addBtn.onclick = () => {
+    try{
+    const name = document.getElementById("nameInput").value;
+    const age = document.getElementById("ageInput").value;
+    }catch(err){
+        console.log(err);
+    }
+}
