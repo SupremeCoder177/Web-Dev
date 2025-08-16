4c2b60e192a417e87caf4cb0501f01f2ba4a9dfb
SupremeCoder177 <onlyforworkplsremember@gmail.com>
2025-08-16 14:13:51 +0530

    Learned How to Slide Image in CSS

---
+++ Learning/Image Slider/index.js
@@ -0,0 +1,10 @@
+const nextBtn = document.getElementById("next");
+const previousBtn = document.getElementById("previous");
+
+nextBtn.onclick = () => {
+    const deer_pic = document.getElementById("deer");
+    const mountain_pic = document.getElementById("mountains");
+    const fog_pic = document.getElementById("fog");
+
+    
+}
