4211d58a141f5c7c3fe2b25cd5d772efd7581b8e
Skullfire <onlyforworkplsremember@gmail.com>
2025-10-03 19:39:39 +0530

    My first javascript styling experience, so cool

---
+++ Learning/Starry Sky/index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let stars = document.querySelector('.stars');
+    for(let i = 0; i < 100; i++){
+        let star = document.createElement('div');
+        star.classList.add('star');
+
+        let x = Math.random() * 100;
+        let y = Math.random() * 100;
+
+        star.style.top = `${y}vh`;
+        star.style.left = `${x}vw`;
+        star.style.position = 'absolute';
+
+        let size = Math.random() * 3 + 1;
+
+        star.style.width = `${size}px`;
+        star.style.height = `${size}px`;
+
+        const duration = Math.random() * 3 + 1;
+        const delay = Math.random() * 2;
+        star.style.animationDuration = `${duration}s`;
+        star.style.animationDelay = `${delay}s`;
+
+        console.log("adding...")
+
+        stars.appendChild(star);
+    }
+})
