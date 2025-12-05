10959339f2ff4ed1dafdc9196dcb96111d79ad0d
Skullfire <onlyforworkplsremember@gmail.com>
2025-12-05 12:17:32 +0530

    Learned React (kinda)

---
+++ Projects/Clothing-Website/src/Components/SearchBar.jsx
@@ -0,0 +1,13 @@
+export function SearchBar(){
+
+    return (
+        <div>
+            <input type="text" className="search-input"/>
+            <button>
+                
+            </button>
+        </div>
+    );
+
+
+}
